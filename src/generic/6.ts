f53f3f3375866b361d14abc1c32581c9b3a5c83f
liussi <125599487+liussi@users.noreply.github.com>
2023-10-24 15:49:02 +0300

    fix

--- src/generic/6.ts
+++ src/generic/6.ts
@@ -6,7 +6,6 @@
 
   Виправте тип у аргументі функції так, щоб не було помилок типу.
 */
-
 type User = {
   name: string;
   surname: string;
@@ -14,10 +13,20 @@ type User = {
   password: string;
 }
 
-function createOrUpdateUser(initialValues: User) {
-  // Оновлення користувача
+function createOrUpdateUser(toUpdate: Partial<User> ): User {
+  const defaultUser: User = {
+    surname:'',
+    password: '',
+    email: '',
+    name:''
+  };
+  return { ...defaultUser, ...toUpdate };
 }
 
-createOrUpdateUser({ email: 'user@mail.com', password: 'password123' });
+
+const updatedUser = createOrUpdateUser({ email: 'user@mail.com', password: 'password123' });
+
+console.log(updatedUser);
+
 
 export {};
